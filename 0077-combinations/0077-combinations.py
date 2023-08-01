7b4771aee7f03beefdd68f6d237479fee7dcca99
Arkojyoti Dey <98301681+Arkojyoti13@users.noreply.github.com>
2023-08-01 14:42:45 +0530

    Time: 273 ms (79.43%), Space: 18.2 MB (90.06%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,4 +1,14 @@
-import itertools
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        return itertools.combinations(range(1,n+1),k)
+        result = []
+        def dfs(combination,start,k):
+            if k == 0:
+                result.append(combination[:])
+                return
+            # Recursively call dfs on unseen words
+            for i in range(start,n+1):
+                combination.append(i)
+                dfs(combination,i+1,k-1)
+                combination.pop()
+        dfs([],1,k)
+        return result
