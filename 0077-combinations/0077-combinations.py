cea18bf8fa7b3b30533dbf3a7c7af1ecaed9dada
Arkojyoti Dey <98301681+Arkojyoti13@users.noreply.github.com>
2023-08-01 14:43:20 +0530

    Time: 263 ms (80.34%), Space: 18.3 MB (34.56%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,14 +1,17 @@
+
 class Solution:
-    def combine(self, n: int, k: int) -> List[List[int]]:
-        result = []
-        def dfs(combination,start,k):
-            if k == 0:
-                result.append(combination[:])
-                return
-            # Recursively call dfs on unseen words
-            for i in range(start,n+1):
-                combination.append(i)
-                dfs(combination,i+1,k-1)
-                combination.pop()
-        dfs([],1,k)
-        return result
+  def combine(self, n: int, k: int) -> List[List[int]]:
+    ans = []
+
+    def dfs(s: int, path: List[int]) -> None:
+      if len(path) == k:
+        ans.append(path.copy())
+        return
+
+      for i in range(s, n + 1):
+        path.append(i)
+        dfs(i + 1, path)
+        path.pop()
+
+    dfs(1, [])
+    return ans
